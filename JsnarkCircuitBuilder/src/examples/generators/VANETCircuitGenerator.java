cb00ca07c7d6ba8859a1094167b2db5ec8c66345
huangjunqin <junqin.huang@qq.com>
2020-07-07 22:00:41 +0800

    build vanet circuit (not tested)

--- JsnarkCircuitBuilder/src/examples/generators/VANETCircuitGenerator.java
+++ JsnarkCircuitBuilder/src/examples/generators/VANETCircuitGenerator.java
@@ -26,7 +26,9 @@ public class VANETCircuitGenerator extends CircuitGenerator{
     private Wire[] inputMessage;
     private Wire[] randomness;
     private Wire[] cipherText;
-    private LongElement rsaModulus;
+    private Wire[] publicKey;
+    private LongElement raModulus; // mpk, (modulus||exponent) is the public key, here the exponent always is 65537, i.e., 0x10001.
+    private LongElement rsaModulus; // pk
     private LongElement signature;
 
     private RSAEncryptionV1_5_Gadget rsaEncryptionV1_5_Gadget;
@@ -43,14 +45,34 @@ public class VANETCircuitGenerator extends CircuitGenerator{
 
     @Override
     protected void buildCircuit() {
+        /**
+         * here, inputMessage and mpk are common knowledge, pk, sk, cert are private witnesses.
+         */
 
-        inputMessage = createProverWitnessWireArray(plainTextLength); // in bytes
-        for(int i = 0; i < plainTextLength;i++){
-            inputMessage[i].restrictBitLength(8);
+        // verify the signature
+        publicKey = createProverWitnessWireArray(rsaKeyLength/4); // private witness, rsaModulus
+        for(int i = 0; i < rsaKeyLength/4;i++){ // in bytes
+            publicKey[i].restrictBitwidth();
         }
+        sha2Gadget = new SHA256Gadget(publicKey, 8, publicKey.length, false, true);
+        Wire[] digest = sha2Gadget.getOutputWires();
+        raModulus = createLongElementInput(rsaKeyLength); // common knowledge
 
-        randomness = createProverWitnessWireArray(RSAEncryptionV1_5_Gadget
-                .getExpectedRandomnessLength(rsaKeyLength, plainTextLength));
+        // since the signature is provided as a witness, verify some properties about it
+        signature = createLongElementProverWitness(rsaKeyLength); // private witness
+        signature.restrictBitwidth();
+        signature.assertLessThan(raModulus); // might not be really necessary in that case
+
+        // verify
+        rsaSigVerificationV1_5_Gadget = new RSASigVerificationV1_5_Gadget(
+                raModulus, digest, signature, rsaKeyLength);
+        makeOutput(rsaSigVerificationV1_5_Gadget.getOutputWires()[0],
+                "Is Signature valid?");
+
+        // encryption
+        inputMessage = createInputWireArray(plainTextLength); // in bytes
+
+        randomness = createProverWitnessWireArray(RSAEncryptionV1_5_Gadget.getExpectedRandomnessLength(rsaKeyLength, plainTextLength)); // private witness, similar to private key
         // constraints on the randomness vector are checked later.
 
 
@@ -69,16 +91,10 @@ public class VANETCircuitGenerator extends CircuitGenerator{
          *
          **/
 
-        rsaModulus = createLongElementInput(rsaKeyLength);
+        rsaModulus = createLongElementProverWitness(rsaKeyLength); // private witness, public key
+        rsaModulus.restrictBitwidth();
 
-        // The modulus can also be hardcoded by only changing the following
-
-        // rsaModulus = new LongElement(Util.split(new
-        // BigInteger("f0dac4df56945ec31a037c5b736b64192f14baf27f2036feb85dfe45dc99d8d3c024e226e6fd7cabb56f780f9289c000a873ce32c66f4c1b2970ae6b7a3ceb2d7167fbbfe41f7b0ed7a07e3c32f14c3940176d280ceb25ed0bf830745a9425e1518f27de822b17b2b599e0aea7d72a2a6efe37160e46bf7c78b0573c9014380ab7ec12ce272a83aaa464f814c08a0b0328e191538fefaadd236ae10ba9cbb525df89da59118c7a7b861ec1c05e09976742fc2d08bd806d3715e702d9faa3491a3e4cf76b5546f927e067b281c25ddc1a21b1fb12788d39b27ca0052144ab0aad7410dc316bd7e9d2fe5e0c7a1028102454be9c26c3c347dd93ee044b680c93cb",
-        // 16), LongElement.CHUNK_BITWIDTH));
-
-        rsaEncryptionV1_5_Gadget = new RSAEncryptionV1_5_Gadget(rsaModulus, inputMessage,
-                randomness, rsaKeyLength);
+        rsaEncryptionV1_5_Gadget = new RSAEncryptionV1_5_Gadget(rsaModulus, inputMessage, randomness, rsaKeyLength);
 
         // since the randomness vector is a witness in this example, verify any needed constraints
         rsaEncryptionV1_5_Gadget.checkRandomnessCompliance();
@@ -87,14 +103,13 @@ public class VANETCircuitGenerator extends CircuitGenerator{
 
         // do some grouping to reduce VK Size
         cipherText = new WireArray(cipherTextInBytes).packWordsIntoLargerWords(8, 30);
-        makeOutputArray(cipherText,
-                "Output cipher text");
+        makeOutputArray(cipherText, "Output cipher text");
 
     }
 
     @Override
     public void generateSampleInput(CircuitEvaluator evaluator) {
-
+        // simulate input message
         String msg = "";
         for (int i = 0; i < inputMessage.length; i++) {
 
@@ -104,13 +119,12 @@ public class VANETCircuitGenerator extends CircuitGenerator{
         System.out.println("PlainText:" + msg);
 
         try {
+            // generate (mpk, msk) for RA
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(rsaKeyLength, new SecureRandom());
+            KeyPair raKeyPair = keyGen.generateKeyPair();
 
-            // to make sure that the implementation is working fine,
-            // encrypt with the underlying java implementation for RSA
-            // Encryption in a sample run,
-            // extract the randomness (after decryption manually), then run the
-            // circuit with the extracted randomness
-
+            // generate (pk, sk) for vehicle
             SecureRandom random = new SecureRandom();
             KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
             generator.initialize(rsaKeyLength, random);
@@ -118,12 +132,34 @@ public class VANETCircuitGenerator extends CircuitGenerator{
             Key pubKey = pair.getPublic();
             BigInteger modulus = ((RSAPublicKey) pubKey).getModulus();
 
+            // CertGen(msk, pk) -> cert
+            Signature signature = Signature.getInstance("SHA256withRSA");
+            signature.initSign(raKeyPair.getPrivate());
+            byte[] message = modulus.toString(16).getBytes();
+            // publicKey wire array
+            for (int i = 0; i < message.length; i++){
+                evaluator.setWireValue(publicKey[i], message[i])
+            }
+            signature.update(message);
+            byte[] sigBytes = signature.sign();
+            byte[] signaturePadded = new byte[sigBytes.length + 1];
+            System.arraycopy(sigBytes, 0, signaturePadded, 1, sigBytes.length);
+            signaturePadded[0] = 0;
+            BigInteger raModulus = ((RSAPublicKey) raKeyPair.getPublic()).getModulus();
+            BigInteger sig = new BigInteger(signaturePadded);
+
+            evaluator.setWireValue(this.raModulus, raModulus, LongElement.CHUNK_BITWIDTH);
+            evaluator.setWireValue(this.signature, sig, LongElement.CHUNK_BITWIDTH);
+
+            // to make sure that the implementation is working fine,
+            // encrypt with the underlying java implementation for RSA
+            // Encryption in a sample run,
+            // extract the randomness (after decryption manually), then run the
+            // circuit with the extracted randomness
+
             Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-            evaluator.setWireValue(this.rsaModulus, modulus,
-                    LongElement.CHUNK_BITWIDTH);
-
+            evaluator.setWireValue(this.rsaModulus, modulus, LongElement.CHUNK_BITWIDTH);
             Key privKey = pair.getPrivate();
-
             cipher.init(Cipher.ENCRYPT_MODE, pubKey, random);
             byte[] cipherText = cipher.doFinal(msg.getBytes());
 //			System.out.println("ciphertext : " + new String(cipherText));
@@ -131,8 +167,7 @@ public class VANETCircuitGenerator extends CircuitGenerator{
             System.arraycopy(cipherText, 0, cipherTextPadded, 1, cipherText.length);
             cipherTextPadded[0] = 0;
 
-            byte[][] result = RSAUtil.extractRSARandomness1_5(cipherText,
-                    (RSAPrivateKey) privKey);
+            byte[][] result = RSAUtil.extractRSARandomness1_5(cipherText, (RSAPrivateKey) privKey);
             // result[0] contains the plaintext (after decryption)
             // result[1] contains the randomness
 
@@ -159,7 +194,7 @@ public class VANETCircuitGenerator extends CircuitGenerator{
         int keyLength = 2048;
         int msgLength = 3;
         VANETCircuitGenerator generator = new VANETCircuitGenerator(
-                "rsa" + keyLength + "_encryption", keyLength, msgLength);
+                "vanet_rsa" + keyLength, keyLength, msgLength);
         generator.generateCircuit();
         generator.evalCircuit();
         generator.prepFiles();
